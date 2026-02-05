96204e2031e83a55fdaf7f8e649754086f2a9ef3
Crodyto <crodyto@gmail.com>
2026-02-06 00:12:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-const btn = document.getElementById("notifyBtn");
+// Firebase config
+var firebaseConfig = {
+  apiKey: "AIzaSyCAkdsO3-Dzbwv6a4QCuEdhWbCmPNCVXtc",
+  authDomain: "crodytolaunchingsoon.firebaseapp.com",
+  databaseURL: "https://crodytolaunchingsoon-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "crodytolaunchingsoon",
+  storageBucket: "crodytolaunchingsoon.firebasestorage.app",
+  messagingSenderId: "473030269256",
+  appId: "1:473030269256:web:d15d063e6e36d37222fb7d"
+};
 
-btn.addEventListener("click", () => {
-  const phone = document.getElementById("phone").value.trim();
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  if (!phone || phone.length < 10) {
-    alert("Please enter a valid mobile number");
+// Reference to database
+var database = firebase.database();
+
+// Save number
+function saveNumber() {
+  var phoneInput = document.getElementById("phone");
+  var phone = phoneInput.value.trim();
+
+  // validation
+  if (phone.length !== 10 || isNaN(phone)) {
+    alert("Enter a valid 10 digit mobile number");
     return;
   }
 
-  console.log("User Phone Number:", phone);
-  alert("Thanks! We'll notify you soon 🚀");
+  database.ref("launch_users").push({
+    phone: phone,
+    createdAt: new Date().toISOString()
+  });
 
-  document.getElementById("phone").value = "";
-});
+  alert("🔥 You're in! We'll notify you soon.");
+  phoneInput.value = "";
+}
